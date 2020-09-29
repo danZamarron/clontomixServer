a2b0a05513d805dffd58d85670bb76e4eb5810ac
Dan Zamarron <daniel.zamarron23@gmail.com>
2020-09-29 14:56:41 -0500

    frontend

---
+++ public/build/precache-manifest.626fc82e5ec08dc0b3efbdc4a6d8652f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f572dd181c0cc33139242284df64cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b24f13488b8d09c3ae84",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "37b592a8b00a9b37257a",
+    "url": "/static/css/main.be6c490e.chunk.css"
+  },
+  {
+    "revision": "b24f13488b8d09c3ae84",
+    "url": "/static/js/2.b54dc5c2.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.b54dc5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b592a8b00a9b37257a",
+    "url": "/static/js/main.1dbcae90.chunk.js"
+  },
+  {
+    "revision": "eb0218ad86bbd22e691b",
+    "url": "/static/js/runtime-main.dacdfabf.js"
+  }
+]);
